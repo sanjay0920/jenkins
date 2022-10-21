8e169e61e1c062476fda4d3450ec344d497e39a2
sanjay0920 <sanjaybharathis18087@it.ssn.edu.in>
2022-10-21 11:06:36 +0530

    jenkins

---
+++ CalculatorJenkins/CalculatorJenkins/Calci.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorJenkins
+{
+    public class Calci
+    {
+        public int Add(int i, int j)
+        {
+            return i + j;
+        }
+        public int Sub(int i, int j)
+        {
+            return i - j;
+        }
+        public int Mul(int i, int j)
+        {
+            return i * j;
+        }
+    }
+}
